20bf57dde673ae51915a1d550c5d4dbd8d142863
Henrique Jonas <hjhenriquesesi@gmail.com>
2012-03-30 22:10:09 -0300

    Add base directories

---
+++ trinity-system/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrinitySystem::Application.config.secret_token = 'dd08767729347a8870d7a0ceebc711c8b57eb84d5d06b5516fea7e783df1f0ac077c68d40abc83e073ca3ef89926ecea3834cee2ded0d6e55593153a82ef72bc'
